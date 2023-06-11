d2204afe23837a4d7ce4cee9669a82ae48656810
myrules1 <financecapitan@gmail.com>
2023-06-11 14:24:34 -0700

    jihane

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    i = len(my_list)
+    for idx in my_list:
+        print("{}".format(my_list[i-1]))
+        i-=1
+        if i < 0:
+            break
