e1d2b432b6d9ee07883a447042ca76dc27e1fbaa
myrules1 <financecapitan@gmail.com>
2023-06-11 11:43:09 -0700

    rr

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,4 @@
-def print_list_integer(my_list: list):
-    for element in my_list:
-        print(element)
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for i in range(0, len(my_list)):
+        print("{:d}".format(my_list[i]))
